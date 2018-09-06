a175c12453998bebf6af8451c5a3b284184aad29
„Sebastian <bastigeiger@gmail.com>
2018-09-06 22:11:40 +0200

    initial project with firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,14 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+  var config = {
+    apiKey: "AIzaSyD0beW9ahqFVsAVCCd-nk5slTeJEvn8FXg",
+    authDomain: "fitness-app-28f70.firebaseapp.com",
+    databaseURL: "https://fitness-app-28f70.firebaseio.com",
+    projectId: "fitness-app-28f70",
+    storageBucket: "fitness-app-28f70.appspot.com",
+    messagingSenderId: "757589550994"
+  };
+*/
