06bd225654dbcdc26b32e82bdf489d5b96177802
Sebastian Geiger <sngr@methodpark.de>
2018-09-16 16:06:50 +0200

    Route params and Observable switchMap

--- src/health/shared/services/meals/meals.service.ts
+++ src/health/shared/services/meals/meals.service.ts
@@ -5,6 +5,10 @@ import { Store } from 'store';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+
 
 import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
 import { map } from 'rxjs/operator/map';
@@ -30,6 +34,7 @@ export class MealsService {
     Meal[]
     >).do(next => this.store.set('meals', next));
 
+
   constructor(
     private store: Store,
     private db: AngularFireDatabase,
@@ -40,6 +45,13 @@ export class MealsService {
     return this.authService.user.uid;
   }
 
+  getMeal(key: string) {
+    if (!key) return Observable.of({});
+    return this.store.select<Meal[]>('meals')
+      .filter(Boolean)
+      .map(meals => meals.find((meal: Meal) => meal.$key === key));
+  }
+
   addMeal(meal: Meal) {
     return this.db.list(`meals/${this.uid}`).push(meal);
   }
@@ -47,4 +59,5 @@ export class MealsService {
   removeMeal(key: string) {
     return this.db.list(`meals/${this.uid}`).remove(key);
   }
+
 }
