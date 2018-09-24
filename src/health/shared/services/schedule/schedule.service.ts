8ed6d524ef8557e3b2796f2a968ded92190bc4d7
Sebastian Geiger <sngr@methodpark.de>
2018-09-24 18:12:11 +0200

    Schedule controls and date logic

--- src/health/shared/services/schedule/schedule.service.ts
+++ src/health/shared/services/schedule/schedule.service.ts
@@ -7,11 +7,18 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class ScheduleService {
+
   private date$ = new BehaviorSubject(new Date());
 
-  schedule$: Observable<any[]> = this.date$.do((next: any) =>
-    this.store.set('date', next)
-  );
+  schedule$: Observable<any[]> = this.date$
+    .do((next: any) => this.store.set('date', next));
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store
+  ) { }
+
+  updateDate(date: Date) {
+    this.date$.next(date);
+  }
+
 }
