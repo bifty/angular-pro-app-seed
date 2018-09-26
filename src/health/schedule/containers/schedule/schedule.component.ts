5e16bae818c9dd23d2c88acb4c633ce85b8bf47c
Sebastian Geiger <sngr@methodpark.de>
2018-09-26 08:58:02 +0200

    Emitting from stateless components

--- src/health/schedule/containers/schedule/schedule.component.ts
+++ src/health/schedule/containers/schedule/schedule.component.ts
@@ -5,38 +5,54 @@ import { Store } from 'store';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
-import { ScheduleService } from '../../../shared/services/schedule/schedule.service';
+import { ScheduleService, ScheduleItem } from '../../../shared/services/schedule/schedule.service';
 
 @Component({
   selector: 'schedule',
   styleUrls: ['schedule.component.scss'],
   template: `
     <div class="schedule">
-      
+
       <schedule-calendar
         [date]="date$ | async"
-        (change)="changeDate($event)">
+        [items]="schedule$ | async"
+        (change)="changeDate($event)"
+        (select)="changeSection($event)">
       </schedule-calendar>
     </div>
-  `,
+  `
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
+
   date$: Observable<Date>;
+  schedule$: Observable<ScheduleItem[]>;
   subscriptions: Subscription[] = [];
 
-  constructor(private store: Store, private scheduleService: ScheduleService) {}
+  constructor(
+    private store: Store,
+    private scheduleService: ScheduleService
+  ) { }
 
   changeDate(date: Date) {
     this.scheduleService.updateDate(date);
   }
 
+  changeSection(event: any) {
+    this.scheduleService.selectSection(event);
+  }
+
   ngOnInit() {
     this.date$ = this.store.select('date');
+    this.schedule$ = this.store.select('schedule');
 
-    this.subscriptions = [this.scheduleService.schedule$.subscribe()];
+    this.subscriptions = [
+      this.scheduleService.schedule$.subscribe(),
+      this.scheduleService.selected$.subscribe(),
+    ];
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
+
 }
--- src/store.ts
+++ src/store.ts
@@ -7,23 +7,29 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import { User } from './auth/shared/services/auth/auth.service';
 import { Meal } from './health/shared/services/meals/meals.service';
 import { Workout } from './health/shared/services/workouts/workouts.service';
+import { ScheduleItem } from './health/shared/services/schedule/schedule.service';
 
 export interface State {
-  user: User;
-  meals: Meal[];
-  date: Date;
-  workouts: Workout[];
-  [key: string]: any;
+  user: User,
+  meals: Meal[],
+  selected: any,
+  schedule: ScheduleItem[],
+  date: Date,
+  workouts: Workout[],
+  [key: string]: any
 }
 
 const state: State = {
   user: undefined,
   meals: undefined,
+  selected: undefined,
+  schedule: undefined,
   date: undefined,
   workouts: undefined,
 };
 
 export class Store {
+
   private subject = new BehaviorSubject<State>(state);
   private store = this.subject.asObservable().distinctUntilChanged();
 
@@ -38,4 +44,5 @@ export class Store {
   set(name: string, state: any) {
     this.subject.next({ ...this.value, [name]: state });
   }
+
 }
